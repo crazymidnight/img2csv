718be6d32f4082402795be59fc63b726cc2faab0
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-05-27 15:59:21 +0700

    add dcm to rgb converting, saving data to png format

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from pydicom.filereader import dcmread
 from pprint import pprint
+import numpy as np
+from PIL import Image
 
 LUNG_MODE = {"left": -164, "right": 712}
 ROOT_PATH = "../../../diploma/medical_data"
@@ -13,36 +15,43 @@ DATA_PATH = (
 )
 
 # TODO: разобрать эту жесть и сделать исключение не dcm-файлов
-def load_dataset(path: str = ROOT_PATH, folders: tuple = DATA_PATH) -> dict:
+def process_dataset(path: str = ROOT_PATH, folders: tuple = DATA_PATH) -> dict:
     data = {folder: None for folder in folders}
+    
     for folder in folders:
+        if not os.path.exists(f"../data/{folder}"):
+            os.mkdir(f"../data/{folder}/")
         nested = sorted(os.listdir(f"{path}/{folder}"))
-        data[folder] = {
-            key: {
-                img: {
-                    [
-                        dcmread(
+        for key in nested:
+            if not os.path.exists(f"../data/{folder}/{key}/{key.split('_')[-1]}"):
+                os.mkdir(f"../data/{folder}/{key}/{key.split('_')[-1]}")
+            for img in os.listdir(f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/"):
+                if not os.path.exists(f"../data/{folder}/{key}/{key.split('_')[-1]}/CT/"):
+                    os.mkdir(f"../data/{folder}/{key}/{key.split('_')[-1]}/CT/")
+                for x in os.listdir(
+                    f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/{img}"
+                ):
+                    if x[::-1][:4] == "mcd.":
+                        dcm = dcmread(
                             f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/{img}/{x}"
                         )
-                        for x in os.listdir(
-                            f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/{img}"
+                        dcm = dcm.pixel_array.astype(np.uint16)
+                        rgb = to_rgb(dcm)
+                        im = Image.fromarray(rgb)
+                        im.save(
+                            f"../data/{folder}/{key}/{key.split('_')[-1]}/CT/{img}.png",
+                            format="png",
                         )
-                    ]
-                }
-                for img in os.listdir(f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/")
-            }
-            for key in nested
-        }
-    pprint(data)
-    return data
 
 
-def load_data(path=ROOT_PATH):
-    files = sorted(os.listdir(path))
-    print(f"files: {files}")
-    for i in files:
-        file = os.listdir(f"{path}{i}")
-        print(file)
+# Convert dicom to rgb
+def to_rgb(img: np.array) -> np.array:
+
+    img[img < LUNG_MODE["left"]] = LUNG_MODE["left"]
+    img[img > LUNG_MODE["right"]] = LUNG_MODE["right"]
+
+    rgb = (img - LUNG_MODE["left"]) / (LUNG_MODE["right"] - LUNG_MODE["left"]) * 255
+    return rgb.astype(np.uint8)
 
 
 def create_csv():
@@ -50,4 +59,4 @@ def create_csv():
 
 
 if __name__ == "__main__":
-    load_dataset()
+    process_dataset()
