dccbaa42a9b19741c5b776b594d9508605802e7d
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-05-28 12:10:20 +0700

    add creating csv

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pydicom.filereader import dcmread
 from pprint import pprint
 import numpy as np
 from PIL import Image
+import pandas as pd
 
 LUNG_MODE = {"left": -164, "right": 712}
 ROOT_PATH = "../../../diploma/medical_data"
@@ -16,40 +17,48 @@ DATA_PATH = (
 
 # TODO: разобрать эту жесть и сделать исключение не dcm-файлов
 def process_dataset(path: str = ROOT_PATH, folders: tuple = DATA_PATH) -> dict:
-    data = {folder: None for folder in folders}
+    data = {
+        "patient_id": [],
+        "slice_id": [],
+        "x": [],
+        "y": [],
+        "intensity": [],
+        "label": [],
+    }
 
     for folder in folders:
         if not os.path.exists(f"../data/{folder}"):
-            os.mkdir(f"../data/{folder}/")
+            os.makedirs(f"../data/{folder}/")
         nested = sorted(os.listdir(f"{path}/{folder}"))
-        for patiend_id in nested:
-            if not os.path.exists(
-                f"../data/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}"
-            ):
-                os.mkdir(f"../data/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}")
-            for img in os.listdir(
-                f"{path}/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/"
-            ):
+        for key in nested:
+            if not os.path.exists(f"../data/{folder}/{key}/{key.split('_')[-1]}"):
+                os.makedirs(f"../data/{folder}/{key}/{key.split('_')[-1]}")
+            for imgs in os.listdir(f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/"):
                 if not os.path.exists(
-                    f"../data/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/"
+                    f"../data/{folder}/{key}/{key.split('_')[-1]}/CT/"
                 ):
-                    os.mkdir(
-                        f"../data/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/"
-                    )
-                for x in os.listdir(
-                    f"{path}/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/{img}"
+                    os.makedirs(f"../data/{folder}/{key}/{key.split('_')[-1]}/CT/")
+                for img in os.listdir(
+                    f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/{imgs}"
                 ):
-                    if x[::-1][:4] == "mcd.":
+                    if img[::-1][:4] == "mcd.":
                         dcm = dcmread(
-                            f"{path}/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/{img}/{x}"
+                            f"{path}/{folder}/{key}/{key.split('_')[-1]}/CT/{imgs}/{img}"
                         )
                         dcm = dcm.pixel_array.astype(np.uint16)
                         rgb = to_rgb(dcm)
-                        im = Image.fromarray(rgb)
-                        im.save(
-                            f"../data/{folder}/{patiend_id}/{patiend_id.split('_')[-1]}/CT/{img}.png",
-                            format="png",
-                        )
+
+                        for x in range(512):
+                            for y in range(512):
+                                data = create_row(
+                                    data=data,
+                                    patient_id=key.split("_")[-1],
+                                    slice_id=f"{imgs}-{img}",
+                                    x=x,
+                                    y=y,
+                                    intesity=rgb[x][y],
+                                )
+    return data
 
 
 # Convert dicom to rgb
@@ -62,9 +71,21 @@ def to_rgb(img: np.array) -> np.array:
     return rgb.astype(np.uint8)
 
 
+def create_row(data, patient_id, slice_id, x, y, intesity):
+    data["patient_id"].append(patient_id)
+    data["slice_id"].append(slice_id)
+    data["x"].append(x)
+    data["y"].append(y)
+    data["intensity"].append(intesity)
+    data["label"].append(0)
+    return data
+
+
 def create_csv():
     pass
 
 
 if __name__ == "__main__":
-    process_dataset()
+    data = process_dataset()
+    df = pd.DataFrame(data)
+    df.to_cvs("data.csv")
